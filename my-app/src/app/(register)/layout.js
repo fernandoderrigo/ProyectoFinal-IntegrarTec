13cd9bf47ee1fd6bab60b43bce7c7849220c3142
NahuelBMiras <nahuelbasailmiras@gmail.com>
2024-09-12 20:26:54 -0300

    estilos 0.4

--- my-app/src/app/(register)/layout.js
+++ my-app/src/app/(register)/layout.js
@@ -5,7 +5,7 @@ export default function LayoutOpen({ children }) {
   return (
     <>
       <HeaderProfile />
-      <main className="mt-16 mb-40">{children}</main>
+      <main className="mt-16 pb-40">{children}</main>
       <footer className="fixed bottom-0 w-full">
         <NavMicrophoneOnly />
       </footer>
--- my-app/src/components/common/navigation-bar/NavMicrophoneOnly.jsx
+++ my-app/src/components/common/navigation-bar/NavMicrophoneOnly.jsx
@@ -1,12 +1,26 @@
+'use client';
+
+import React from 'react';
 import { MdKeyboardVoice } from 'react-icons/md';
+import useMicrophone from '@/hooks/useMicrophone';
 
 export default function NavMicrophoneOnly() {
+  const { startListening, stopListening, isListening } = useMicrophone();
+
+  const handleClick = () => {
+    if (isListening) {
+      stopListening();
+    } else {
+      startListening();
+    }
+  };
+
   return (
     <nav className="w-full">
       <ul className="grid w-full grid-cols-5 gap-4 px-4 pb-1 pt-7 bg-gradient-to-t from-indigo-500 to-transparent">
         <li className="flex items-center justify-center col-start-3">
-          <button>
-            <MdKeyboardVoice className="text-5xl " />
+          <button onClick={handleClick}>
+            <MdKeyboardVoice className={`text-5xl ${isListening ? 'text-green-500' : 'text-white'}`} />
           </button>
         </li>
       </ul>
